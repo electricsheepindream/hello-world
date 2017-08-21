3f55b333fefad3a81d1b14e0a42ba1a2ea179be9
esid <electricsheepindream@gmail.com>
2017-08-21 10:56:30 +0800

    hello

--- music/robot.py
+++ music/robot.py
@@ -1,13 +1,29 @@
 from werobot import WeRoBot
-
-
+from urllib import request, parse
+import json
 robot = WeRoBot(enable_session=False,
                 token='esid',
                 APP_ID='wxc6ec8eda771e9cf2',
                 APP_SECRET='99e8d052e83864de0df9c973382b5ffd'
                 )
+msg = '淄博'
+msg = parse.quote(str(msg))
+doc = request.urlopen(url='http://www.sojson.com/open/api/weather/json.shtml?city={city}'.format(city=msg)).read()
+res = doc.decode('utf-8')
+data = json.loads(res)
+con = ''
+for i in range(5):
+    weather = data['data']['forecast'][i]
+    out = '{data}:日出时间:{sunrise},{high},{low},日落：{sunset},天气状况：{tianqi}建议:{notice}\n'.format\
+        (data=weather['date'], sunrise=weather['sunrise'], high=weather['high'], low=weather['low'], sunset=weather['sunset'],\
+         tianqi=weather['type'], notice=weather['notice'])
+    con += out
 
 
-@robot.handler
+@robot.text
 def hello(message):
-    return '傻瓜'
+    if message == '天气':
+        return con
+
+
+
